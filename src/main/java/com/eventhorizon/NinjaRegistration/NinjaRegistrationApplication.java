8b4dbfb79a280e1f9fc7a54f0e2186a54d53badc
marcos <mgmarcosgrubert@gmail.com>
2026-02-22 23:38:14 -0300

    feat: first commit

---
+++ src/main/java/com/eventhorizon/NinjaRegistration/NinjaRegistrationApplication.java
@@ -0,0 +1,13 @@
+package com.eventhorizon.NinjaRegistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NinjaRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NinjaRegistrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eventhorizon/NinjaRegistration/NinjaRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventhorizon.NinjaRegistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NinjaRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
