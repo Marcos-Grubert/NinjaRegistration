8bf6665b1ce65afb67ee29d373c79f09a89797ae
marcos <mgmarcosgrubert@gmail.com>
2026-02-25 22:37:37 -0300

    feat: implements funcions showNinjas and showNinjaPerId and create ninjaRepository dependecy in Ninja service

--- src/main/java/com/eventhorizon/NinjaRegistration/Ninja/NinjaService.java
+++ src/main/java/com/eventhorizon/NinjaRegistration/Ninja/NinjaService.java
@@ -1,4 +1,25 @@
 package com.eventhorizon.NinjaRegistration.Ninja;
 
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class NinjaService {
+    private NinjaRepository ninjaRepository;
+
+    public NinjaService(NinjaRepository ninjaRepository) {
+        this.ninjaRepository = ninjaRepository;
+    }
+
+    //Show all ninjas
+    public List<NinjaModel> showNinjas(){
+        return ninjaRepository.findAll();
+    }
+
+    public NinjaModel showNinjaPerId(Long id){
+        Optional<NinjaModel> ninja = ninjaRepository.findById(id);
+        return ninja.orElse(null);
+    }
 }
