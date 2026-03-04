7455620b1faa233529459cd8b20f44ad6072e263
marcos <mgmarcosgrubert@gmail.com>
2026-03-03 23:50:23 -0300

    feat: implements ui class and functions for show ninjas and delete

--- src/main/java/com/eventhorizon/NinjaRegistration/Ninja/ControllerUi.java
+++ src/main/java/com/eventhorizon/NinjaRegistration/Ninja/ControllerUi.java
@@ -1,4 +1,35 @@
 package com.eventhorizon.NinjaRegistration.Ninja;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/ninja/ui")
 public class ControllerUi {
+    private final NinjaService ninjaService;
+
+    public ControllerUi(NinjaService ninjaService) {
+        this.ninjaService = ninjaService;
+    }
+
+    //Mostrar ninjas
+    @GetMapping("/showAll")
+    public String showAll(Model model) {
+        List<NinjaDTO> ninjas = ninjaService.showAll();
+        model.addAttribute("ninjas", ninjas);
+        return "showNinjas"; // tem que retornar o nome da pagina que renderiza
+    }
+
+    //delete ninja by id
+    @GetMapping("/delete/{id}")
+    public String deleteById(@PathVariable Long id){
+        ninjaService.deleteById(id);
+        return "redirect:/ninja/ui/showAll";
+    }
 }
